bebc7929acff272e05755191ee9fea3f91c1c4b5
Jhojan Silva <jhojansilva0302@gmail.com>
2026-04-30 17:24:08 -0500

    Arquitectura profesional implementada: Header, Footer y sistema de diseño

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/layout/header/header';
+import { Footer } from './shared/components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
   protected readonly title = signal('MovieNexus');
 }
