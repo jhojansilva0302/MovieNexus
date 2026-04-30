20546b313e1be801bb79141d9a53ef8b0ca15e73
Jhojan Silva <jhojansilva0302@gmail.com>
2026-04-30 17:05:52 -0500

    Proyecto MovieNexus inicializado

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+
+    tmdbApiUrl: 'https://api.themoviedb.org/3',
+
+    tmdbImageUrl: 'https://image.tmdb.org/t/p',
+
+    tmdbApiKey: 'ccf3907854534f675d18ad5201cbea00',
+
+    tmdbAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2YzOTA3ODU0NTM0ZjY3NWQxOGFkNTIwMWNiZWEwMCIsIm5iZiI6MTcyNDM1NDM0ODU0NSwiZWF0IjoxNzI0MzU0MzQ4NTQ1LCJqdGkiOiI0NDM4NGU4NC03NDI5LTRhNDYtYjliMS03NTM1Y2ZkODUzOTYifQ.cbH91cf54i3dXvZrtyheCb5cAskJ5kenclifGDduU-VY'
+};
