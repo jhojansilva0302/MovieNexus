66b17e4181494b2a15898bf8386671782be5eecd
Jhojan Silva <jhojansilva0302@gmail.com>
2026-05-20 16:21:29 -0500

    feat: implementar scroll infinito usando intersection observer en el home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { CommonModule, DecimalPipe } from '@angular/common';
+import { Component, inject, OnInit, signal, AfterViewInit, ElementRef, ViewChild, PLATFORM_ID } from '@angular/core';
+import { CommonModule, DecimalPipe, isPlatformBrowser } from '@angular/common';
 import { MovieService } from '../../core/services/movie.service';
 import { HeroComponent } from './components/hero/hero';
 import { MovieCard } from '../../shared/components/movie-card/movie-card';
@@ -12,13 +12,21 @@ import { Movie } from '../../core/models/movie.model';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements OnInit {
+export class Home implements OnInit, AfterViewInit {
   private movieService = inject(MovieService);
+  private platformId = inject(PLATFORM_ID);
 
-  // Signals para las categorías de películas
+  // Marcamos un elemento del HTML para observarlo
+  @ViewChild('infiniteAnchor') infiniteAnchor!: ElementRef;
+
+  // Signals para las categorías de películas y el Infinite Scroll
   featuredMovie = signal<Movie | null>(null);
   popularMovies = signal<Movie[]>([]);
   topRatedMovies = signal<Movie[]>([]);
+  
+  catalogMovies = signal<Movie[]>([]);
+  currentPage = signal(1);
+  isFetchingNextPage = signal(false);
 
   ngOnInit(): void {
     // 1. Películas en Tendencia (para el Hero y opcionalmente una lista)
@@ -44,4 +52,38 @@ export class Home implements OnInit {
       }
     });
   }
+
+  ngAfterViewInit(): void {
+    // 3. Solo configuramos el observador en el navegador (SSR Safety)
+    if (isPlatformBrowser(this.platformId)) {
+      this.initInfiniteScroll();
+    }
+  }
+
+  private initInfiniteScroll(): void {
+    const observer = new IntersectionObserver((entries) => {
+      // 4. Si el ancla entra en el campo de visión y no estamos cargando...
+      if (entries[0].isIntersecting && !this.isFetchingNextPage()) {
+        this.loadMoreMovies();
+      }
+    }, { rootMargin: '200px' }); // 'rootMargin' permite cargar 200px antes de llegar al final
+
+    observer.observe(this.infiniteAnchor.nativeElement);
+  }
+
+  loadMoreMovies(): void {
+    this.isFetchingNextPage.set(true);
+    this.movieService.getPopularMovies(this.currentPage()).subscribe({
+      next: (data) => {
+        // 5. Inmutabilidad: Concatenamos los resultados usando el operador spread [...]
+        this.catalogMovies.set([...this.catalogMovies(), ...data.results]);
+        this.currentPage.update(p => p + 1);
+        this.isFetchingNextPage.set(false);
+      },
+      error: () => {
+        this.isFetchingNextPage.set(false);
+      }
+    });
+  }
 }
+
